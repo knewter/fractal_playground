a51e49715a0b2cf893fed46209344b093363c5b3
Josh Adams <josh@isotope11.com>
2015-06-16 19:02:19 -0500

    Move LSystem to its own module

--- test/fractal_playground_test.exs
+++ test/fractal_playground_test.exs
@@ -1,26 +1,3 @@
-defmodule LSystem do
-  def transform(string, rules) do
-    transform(String.graphemes(string), rules, "")
-  end
-
-  def run(input, rules, steps) do
-    (1..steps)
-    |> Enum.reduce([input], fn(_step, acc=[head|_rest2]) ->
-      [transform(head, rules) | acc]
-    end)
-    |> Enum.reverse
-  end
-
-  defp transform([], _rules, acc), do: acc
-  defp transform([first_char|rest], rules, acc) do
-    transform(rest, rules, acc <> transform_char(first_char, rules))
-  end
-
-  defp transform_char(char, rules) do
-    rules[char] || char
-  end
-end
-
 defmodule FractalPlaygroundTest do
   use ExUnit.Case
 
